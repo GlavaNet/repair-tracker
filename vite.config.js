09ee4898634e3b4c4397d0d5dd10321e110df8c0
GlavaNet <christopher.glavan@gmx.com>
2025-05-20 01:23:40 -0700

    Update vite config to handle jsx in js files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repair-tracker/' // Set to your repo name
+  base: '/repair-tracker/',
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
